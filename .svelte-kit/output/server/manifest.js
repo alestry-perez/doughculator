abc44dc87edc28ee58a1ba8bcfab0cd635d2535b
alestry-perez <alestry.perez@hyperisland.se>
2026-03-06 11:14:28 +0100

    set favicon

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "doughculator/_app",
-	assets: new Set(["robots.txt"]),
-	mimeTypes: {".txt":"text/plain"},
+	assets: new Set(["favicon.svg","robots.txt"]),
+	mimeTypes: {".svg":"image/svg+xml",".txt":"text/plain"},
 	_: {
-		client: {start:"_app/immutable/entry/start.D9Byj0M5.js",app:"_app/immutable/entry/app.BxJol6vr.js",imports:["_app/immutable/entry/start.D9Byj0M5.js","_app/immutable/chunks/BrBIV72O.js","_app/immutable/chunks/BL6YA9ew.js","_app/immutable/chunks/oRnSefnY.js","_app/immutable/chunks/vH7urwBu.js","_app/immutable/entry/app.BxJol6vr.js","_app/immutable/chunks/BL6YA9ew.js","_app/immutable/chunks/DOSRN90_.js","_app/immutable/chunks/BrogzO-e.js","_app/immutable/chunks/vH7urwBu.js","_app/immutable/chunks/nHT0CA0i.js","_app/immutable/chunks/Ca8BCUAG.js","_app/immutable/chunks/oRnSefnY.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.CiUa7-AV.js",app:"_app/immutable/entry/app.D5C4QZQl.js",imports:["_app/immutable/entry/start.CiUa7-AV.js","_app/immutable/chunks/CDhFNs7Y.js","_app/immutable/chunks/BL6YA9ew.js","_app/immutable/chunks/oRnSefnY.js","_app/immutable/chunks/vH7urwBu.js","_app/immutable/entry/app.D5C4QZQl.js","_app/immutable/chunks/BL6YA9ew.js","_app/immutable/chunks/DOSRN90_.js","_app/immutable/chunks/BrogzO-e.js","_app/immutable/chunks/vH7urwBu.js","_app/immutable/chunks/nHT0CA0i.js","_app/immutable/chunks/Ca8BCUAG.js","_app/immutable/chunks/oRnSefnY.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CcGxKMny.js","_app/immutable/chunks/BrogzO-e.js","_app/immutable/chunks/BL6YA9ew.js","_app/immutable/chunks/DOSRN90_.js","_app/immutable/chunks/BrBIV72O.js","_app/immutable/chunks/oRnSefnY.js","_app/immutable/chunks/vH7urwBu.js"];
+export const imports = ["_app/immutable/nodes/1.Dl6GyE-N.js","_app/immutable/chunks/BrogzO-e.js","_app/immutable/chunks/BL6YA9ew.js","_app/immutable/chunks/DOSRN90_.js","_app/immutable/chunks/CDhFNs7Y.js","_app/immutable/chunks/oRnSefnY.js","_app/immutable/chunks/vH7urwBu.js"];
 export const stylesheets = [];
 export const fonts = [];
